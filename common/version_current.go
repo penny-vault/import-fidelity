0e0fd4501d178cc8062e1dafebb408b944476db4
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-06-07 23:50:32 -0700

    fix version number

--- common/version_current.go
+++ common/version_current.go
@@ -18,8 +18,8 @@ package common
 // CurrentVersion represents the current build version.
 // This is the only one in the system
 var CurrentVersion = Version{
-	Major:  1,
-	Minor:  0,
+	Major:  0,
+	Minor:  3,
 	Patch:  0,
 	Suffix: "dev",
 }
